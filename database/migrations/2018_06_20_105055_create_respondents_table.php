d375bc93b27ae17306ae500af6f336ff320a4e1b
Thembo Charles Lwanga <ashley7520charles@gmail.com>
2018-06-26 16:39:17 +0300

    Removes the unique phone number

--- database/migrations/2018_06_20_105055_create_respondents_table.php
+++ database/migrations/2018_06_20_105055_create_respondents_table.php
@@ -17,15 +17,11 @@ class CreateRespondentsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->integer('district_id')->unsigned();
-<<<<<<< HEAD:database/migrations/2018_06_20_105055_create_respondents_table.php
-=======
             $table->string('gender');
             $table->string('address');
-            $table->string('phone_number')->unique();
+            $table->string('phone_number');
             $table->string('email_address')->nullable();
             $table->timestamps();
-
->>>>>>> master:database/migrations/2018_06_20_105055_create_respondents_table.php
             $table->foreign('district_id')->references('id')->on('districts')->onUpdate('cascade')->onDelete('cascade');
         });
     }
