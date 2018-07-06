e54df20a8dee52f30b42891eff911ea3b0cc146d
Thembo Charles Lwanga <ashley7520charles@gmail.com>
2018-07-06 10:57:25 +0300

    Adds Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
     <!-- Scripts -->
    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-    <link rel="icon" href="{{asset('img/logo.png')}}"/>
+    <link rel="icon" href="{{asset('img/favicon.png')}}"/>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
